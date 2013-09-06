5d9e680fa485c459a2a97374d56301a10b008059
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-09-06 13:51:10 +0200

    added chamfered washer base

--- freecad/washer.py
+++ freecad/washer.py
@@ -1,4 +1,3 @@
-#bases
 import Part
 
 def washer1(params,document):
@@ -11,6 +10,20 @@ def washer1(params,document):
 	part = document.addObject("Part::Feature",name)
 	outer = Part.makeCylinder(d2,s)
 	inner = Part.makeCylinder(d1,s)
-	part.Shape = outer.cut(inner)
+	shape = outer.cut(inner)
 
-bases = {'washer1':washer1}
+def washer2(params,document):
+	key = params['key']
+	d1 = params['d1']
+	d2 = params['d2']
+	s = params['s']
+	name = params['name']
+
+	part = document.addObject("Part::Feature",name)
+	outer = Part.makeCylinder(d2,s)
+	inner = Part.makeCylinder(d1,s)
+	shape = outer.cut(inner)
+	#guessed size for the chamfer
+	part.Shape = shape.makeChamfer(0.1*d1,shape.Edges[0:1])
+
+bases = {'washer1':washer1, 'washer2':washer2}
