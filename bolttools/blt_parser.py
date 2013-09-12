c41bdfded827c4ddd87d26056a24eede04ec1bc5
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-09-12 18:52:14 +0200

    rewrote blt parser
    following blt 0.2 spec. With much better error checking and a test
    suite. Decided to not pay attention to compatibility, gets a mess
    otherwise.

--- bolttools/blt_parser.py
+++ bolttools/blt_parser.py
@@ -14,29 +14,55 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import yaml
+import os
+from os.path import splitext
+import re
 
-_specification = { 0.1: {
-	"root" : (["collection","parts"],["scad"]),
+_re_angled = re.compile("([^<]*)<([^>]*)")
+
+current_version = 0.2
+
+_specification = {
+	"root" : (["collection","classes"],[]),
 	"collection" : (["author","license","blt-version"],["name","description"]),
-	"scad" : (["base-file","base-functions"],[]),
-	"part" : (["standard","name","base","target-args"],["status","replaces","description","literal-args","url","table","notes"]),
-	"name" : (["template"],["parameters"]),
-	"table" : (["columns","data"],[])
-	},
+	"class" : (["naming","source"],["drawing","description","standard","status","parameters","url","notes"]),
+	"naming" : (["template"],["placeholder"]),
+	"parameters" : ([],["literal","free","tables","types"]),
+	"table" : (["index","columns","data"],[])
 }
 
-_defaults = { 0.1: {
-	"part" : [("status","active")],
-	},
-}
+class VersionError(Exception):
+	def __init__(self,version):
+		self.version = version
+	def __str__(self):
+		return "Unknown version. File: %f Current: %f" % \
+			(self.version,current_version)
 
-_unify = { 0.1: {
-	"scad" : ["base-file"],
-	"part" : ["standard","replaces"]
-	}
-}
+class UnknownFieldError(Exception):
+	def __init__(self,fieldname):
+		self.field = fieldname
+	def __str__(self):
+		return "Unknown Field: %s" % self.field
 
-def check_dict(array,name,spec):
+class MissingFieldError(Exception):
+	def __init__(self,fieldname):
+		self.field = fieldname
+	def __str__(self):
+		return "Missing Field: %s" % self.field
+
+class MalformedRepositoryError(Exception):
+	def __init__(self,msg):
+		self.msg = msg
+	def __str(self):
+		return self.msg
+
+class MalformedCollectionError(Exception):
+	def __init__(self,msg):
+		self.msg = msg
+	def __str(self):
+		return self.msg
+
+def check_dict(array,spec):
 	man = spec[0][:]
 	opt = spec[1][:]
 	for key in array.keys():
@@ -45,60 +71,219 @@ def check_dict(array,name,spec):
 		elif key in opt:
 			opt.remove(key)
 		else:
-			print "Warning! Unknown field in %s: %s" % (name,key)
+			raise UnknownFieldError(key)
 	if len(man) > 0:
-		print "Error! Missing mandatory fields in %s: %s" % (name,man)
+		raise MissingFieldError(man)
 
-def check_conformity(coll):
-	version = coll["collection"]["blt-version"]
-	if not version in _specification.keys():
-		print "Error! Unknown Version:",version
-	spec = _specification[version]
-	check_dict(coll,"root",spec["root"])
-	check_dict(coll["collection"],"collection",spec["collection"])
-	if "scad" in coll.keys():
-		check_dict(coll["scad"],"scad",spec["scad"])
-	parts = coll["parts"]
-	for part,i in zip(parts,range(len(parts))):
-		check_dict(part,"part%d"%i,spec["part"])
-		if "table" in part.keys():
-			check_dict(part["table"],"table",spec["table"])
+class BOLTSRepository:
+	def __init__(self,path):
+		self.collections = []
 
-def set_defaults(coll):
-	version = coll["collection"]["blt-version"]
-	default = _defaults[version]
-	for part in coll["parts"]:
-		for key,value in default["part"]:
-			if not key in part.keys():
-				part[key] = value
+		#load collection data
+		for filename in os.listdir(path+"/data"):
+			if splitext(filename) == ".blt":
+				self.collections.append(BOLTCollection(path + "/data/" + filename))
 
-def unify_to_list(coll):
-	"""Replace fields that can be both name or list by lists to ease access"""
-	version = coll["collection"]["blt-version"]
-	unify = _unify[version]
-	for field in unify["scad"]:
-		if field in coll["scad"] and isinstance(coll["scad"][field],str):
-			coll["scad"][field] = [coll["scad"][field]]
+		if not os.path.exists(path + "/drawings"):
+			raise MalformedRepositoryError("drawings folder is missing")
 
-	for part in coll["parts"]:
-		for field in unify["part"]:
-			if field in part and isinstance(part[field],str):
-				part[field] = [part[field]]
+		#load backend data
+		#TODO
 
-def convert_table(coll):
-	"""Convert table data to float and None"""
-	for part in coll["parts"]:
-		if "table" in part:
-			data = part["table"]["data"]
-			for key in data:
-				data[key] = [float(v) if not v == "None" else None for v in data[key]]
+class BOLTSCollection:
+	def __init__(self,bltname):
+		coll = list(yaml.load_all(open(bltname)))
+		if len(coll) == 0:
+			raise MalformedCollectionError(
+					"No YAML document found in file %s" % bltname)
+		if len(coll) > 1:
+			raise MalformedCollectionError(
+					"More than one YAML document found in file %s" % bltname)
+		coll = coll[0]
 
+		version = coll["collection"]["blt-version"]
+		if version != current_version:
+			raise VersionException(version)
 
-def load_collection(filename):
-	coll = list(yaml.load_all(open("blt/" + filename)))[0]
-	check_conformity(coll)
-	set_defaults(coll)
-	unify_to_list(coll)
-	convert_table(coll)
-	return coll
-	
+		#Check Conformity
+		spec = _specification
+		try:
+			check_dict(coll,spec["root"])
+		except (UnknownFieldError, MissingFieldError):
+			print "In file %s, field %s" % (bltname,"root")
+			raise
+		try:
+			check_dict(coll["collection"],spec["collection"])
+		except (UnknownFieldError, MissingFieldError):
+			print "In file %s, field %s" % (bltname,"collection")
+			raise
+		classes = coll["classes"]
+		if not isinstance(classes,list):
+			raise MalformedCollectionError("No class in collection %s"% bltname)
+		for cl,i in zip(classes,range(len(classes))):
+			try:
+				check_dict(cl,spec["class"])
+			except (UnknownFieldError, MissingFieldError):
+				print "In file %s, class %d" % (bltname,i)
+				raise
+			if "tables" in cl.keys():
+				tables = cl["tables"]
+				for table,j in zip(tables,range(len(tables))):
+					try:
+						check_dict(table,spec["table"])
+					except (UnknownFieldError, MissingFieldError):
+						print "In file %s, class %d table %d" % (bltname,i,j)
+						raise
+
+		#parse header
+		header = coll["collection"]
+		self.name = ""
+		if "name" in header:
+			self.name = header["name"]
+
+		self.description = ""
+		if "description" in header:
+			self.description = header["description"]
+
+		self.authors = header["author"]
+		if isinstance(self.authors,str):
+			self.authors = [self.authors]
+
+		self.author_names = []
+		self.author_mails = []
+		for author in self.authors:
+			match = _re_angled.match(author)
+			self.author_names.append(match.group(1).strip())
+			self.author_mails.append(match.group(2).strip())
+
+		self.license = header["license"]
+		match = _re_angled.match(self.license)
+		self.license_name = match.group(1).strip()
+		self.license_url = match.group(2).strip()
+
+		#parse classes
+		self.classes = []
+		for cl in coll["classes"]:
+			self.classes.append(BOLTSClass(cl))
+
+class BOLTSClass:
+	def __init__(self,cl):
+		self.naming = BOLTSNaming(cl["naming"])
+
+		self.drawing = None
+		if "drawing" in cl:
+			self.drawing = cl["drawing"]
+
+		self.description = ""
+		if "description" in cl:
+			self.description = cl["description"]
+
+		self.standard = None
+		self.status = "active"
+		self.replaces = None
+		if "standard" in cl:
+			self.standard = cl["standard"]
+			if "status" in cl:
+				self.status = cl["status"]
+				if not self.status in ["active","withdrawn"]:
+					raise ValueError
+			if "replaces" in cl:
+				self.replaces = cl["replaces"]
+
+		if "parameters" in cl:
+			self.parameters = BOLTSParameters(cl["parameters"])
+		else:
+			self.parameters = BOLTSParameters({})
+
+		self.url = ""
+		if "url" in cl:
+			self.url = cl["url"]
+
+		self.notes = ""
+		if "notes" in cl:
+			self.notes = cl["notes"]
+
+		self.source = cl["source"]
+
+class BOLTSParameters:
+	def __init__(self,param):
+		self.literal = {}
+		if "literal" in param:
+			self.literal = param["literal"]
+
+		self.free = []
+		if "free" in param:
+			self.free = param["free"]
+
+		self.tables = []
+		if "tables" in param:
+			if isinstance(param["tables"],list):
+				for t in param["tables"]:
+					self.tables.append(BOLTSTable(t))
+			else:
+				self.tables.append(BOLTSTable(param["tables"]))
+
+		self.types = {}
+		if "types" in param:
+			self.types = param["types"]
+
+		self.parameters = []
+		self.parameters += self.literal.keys()
+		self.parameters += self.free
+		for t in self.tables:
+			self.parameters.append(t)
+			self.parameters += t.columns
+		#remove duplicates
+		self.parameters = list(set(self.parameters))
+
+		#check types
+		all_types = ["Length (mm)", "Length (in)", "Number",
+			"Bool", "Table Index", "String"]
+		
+		for k,t in self.types.iteritems():
+			if not k in self.parameters:
+				raise ValueError("Unknown parameter in types")
+			if not t in all_types:
+				raise ValueError("Unknown type in types")
+
+		#fill in defaults for types
+		for p in self.parameters:
+			if not p in self.types:
+				self.types[p] = "Length (mm)"
+
+		#check and normalize tables
+		for t in self.tables:
+			t._normalize_and_check_types(self.types)
+
+class BOLTSTable:
+	def __init__(self,table):
+		self.index = table["index"]
+		self.columns = table["columns"]
+		self.data = table["data"]
+
+	def _normalize_and_check_types(self,types):
+		numbers = ["Length (mm)", "Length (in)", "Number"]
+		positive = ["Length (mm)", "Length (in)"]
+		rest = ["Bool", "Table Index", "String"]
+		col_types = [types[col] for col in self.columns]
+		idx = range(len(self.columns))
+		for key in self.data:
+			for i,t in zip(idx,col_types):
+				row = self.data[key]
+				if row[i] == "None":
+					row[i] = None
+				if t in numbers:
+					row[i] = float(row[i])
+				elif not t in rest:
+					raise ValueError("Unknown Type in table")
+				if t in positive and row[i] < 0:
+					raise ValueError("Negative length in table")
+				if t == "Bool":
+					row[i] = bool(row[i])
+
+class BOLTSNaming:
+	def __init__(self,name):
+		self.template = name["template"]
+		self.placeholders = []
+		if "placeholders" in name:
+			self.placeholders = name["placeholders"]
---
+++ bolttools/test.py
@@ -0,0 +1,82 @@
+import blt_parser
+import unittest
+
+class TestRepositoryLoad(unittest.TestCase):
+
+	def test_empty(self):
+		self.assertRaises(OSError,
+				lambda: blt_parser.BOLTSRepository("test_repos/empty"))
+
+	def test_no_collections(self):
+		repo = blt_parser.BOLTSRepository("test_repos/no_collections")
+		self.assertEqual(repo.collections,[])
+
+	def test_slash(self):
+		repo = blt_parser.BOLTSRepository("test_repos/no_collections/")
+		self.assertEqual(repo.collections,[])
+
+	def test_no_drawings(self):
+		self.assertRaises(blt_parser.MalformedRepositoryError,
+				lambda: blt_parser.BOLTSRepository(
+					"test_repos/no_drawings/"))
+
+
+class TestCollectionLoad(unittest.TestCase):
+
+	def test_empty(self):
+		self.assertRaises(blt_parser.MalformedCollectionError,
+				lambda: blt_parser.BOLTSCollection(
+					"test_collections/empty.blt"))
+
+	def test_no_classes(self):
+		self.assertRaises(blt_parser.MissingFieldError,
+				lambda: blt_parser.BOLTSCollection(
+					"test_collections/no_classes.blt"))
+
+	def test_empty_classes(self):
+		self.assertRaises(blt_parser.MalformedCollectionError,
+				lambda: blt_parser.BOLTSCollection(
+					"test_collections/empty_classes.blt"))
+
+	def test_minimal_class(self):
+		coll = blt_parser.BOLTSCollection("test_collections/minimal_class.blt")
+		self.assertEqual(coll.author_names,["Johannes Reinhardt"])
+		self.assertEqual(coll.author_mails,["jreinhardt@ist-dein-freund.de"])
+		self.assertEqual(coll.license_name,"CC-BY-SA")
+		self.assertEqual(coll.license_url,"http://creativecommons.org/licenses/by-sa/3.0/")
+
+		cl = coll.classes[0]
+		self.assertEqual(cl.naming.template,"Partname")
+		self.assertEqual(cl.source,"Invented for testpurposes")
+		self.assertEqual(cl.parameters.free,[])
+
+	def test_parameters(self):
+		coll = blt_parser.BOLTSCollection("test_collections/parameters.blt")
+
+		cl = coll.classes[0]
+		self.assertEqual(cl.parameters.free,['key','l'])
+		self.assertEqual(cl.parameters.free,['key','l'])
+		self.assertEqual(type(cl.parameters.tables[0].data['M2.5'][2]),float)
+
+	def test_type_error(self):
+		#additional parameter name in types
+		self.assertRaises(ValueError,
+				lambda: blt_parser.BOLTSCollection(
+					"test_collections/type_error1.blt"))
+		#unknown type  in types
+		self.assertRaises(ValueError,
+				lambda: blt_parser.BOLTSCollection(
+					"test_collections/type_error2.blt"))
+
+	def test_table_error(self):
+		#negative value for parameter of type length
+		self.assertRaises(ValueError,
+				lambda: blt_parser.BOLTSCollection(
+					"test_collections/table_error1.blt"))
+		#negative value for parameter of type number
+		blt_parser.BOLTSCollection("test_collections/table_error2.blt")
+
+if __name__ == '__main__':
+	unittest.main()
+
+
