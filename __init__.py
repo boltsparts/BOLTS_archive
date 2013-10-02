7bd95df820f5bc82b5eac65af6c1ba18fb709d3a
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-10-02 23:20:26 +0200

    preparations for proper drawings
    added a script that more or less automates generation of drawings and a
    template.

--- __init__.py
+++ __init__.py
@@ -14,3 +14,27 @@ widget = BoltsWidget(repo)
 
 mw = getMainWindow()
 mw.addDockWidget(QtCore.Qt.RightDockWidgetArea, widget)
+
+
+import FreeCAD
+def make_drawing(scale,obj):
+	doc = FreeCAD.ActiveDocument
+	page = doc.addObject("Drawing::FeaturePage","Page")
+	page.Template = join(rootpath,"drawings","template.svg")
+
+
+	#front, side right, side left, rear, top, bottom, iso
+	directions = [(0.,0.,1.),(1.,0.,0.),(-1.,0.,0.),(0.,0.,-1.),(0.,-1.,0.),(0.,1.,0.),(1.,1.,1.)]
+	#x center positions
+	positions = [(110.,100.),(40.,100.),(180.,100.),(250.,100.),(110.,35.),(110.,165.),(215.,35.)]
+	rotations = [0,0,0,0,270,270,0]
+	for i in range(7):
+		view = doc.addObject("Drawing::FeatureViewPart","View%d" % i)
+		view.Source = obj
+		view.Direction = directions[i]
+		view.X = positions[i][0]
+		view.Y = positions[i][1]
+		view.Rotation = rotations[i]
+		view.Scale = scale
+		page.addObject(view)
+
