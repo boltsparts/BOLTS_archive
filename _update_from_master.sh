6263d37b81eb8558affb5bc1aa6491b03a27e47a
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-10-05 12:42:00 +0200

    prepared inclusion of statistics to index page

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -3,5 +3,7 @@ git checkout master -- output
 git rm -rfq html downloads
 mv output/html .
 mv output/downloads/* .
+mv html/statistics.html _includes
 git rm -rfq output
 git add html downloads downloads.html
+git add _includes/statistics.html
