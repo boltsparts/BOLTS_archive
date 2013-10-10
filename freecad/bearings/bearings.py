0d37d331590218eef196b9eebdc2bc94773aa13e
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-10-10 22:13:50 +0200

    big relicensing to LGPL 2.1+

--- freecad/bearings/bearings.py
+++ freecad/bearings/bearings.py
@@ -1,7 +1,21 @@
 # coding=utf8
-#2013  Javier Martínez García en Linux Mint KDE 14 <jaeco@gmx.com>
-#2013  Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
-#LGPL
+#BOLTS - Open Library of Technical Specifications
+#Copyright (C) 2013 Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
+#Copyright (C) 2013 Javier Martínez García <jaeco@gmx.com>
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or any later version.
+#
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 import Part
 import math
--- openscad/common/conf.scad
+++ openscad/common/conf.scad
@@ -1,3 +1,21 @@
+/*
+ * BOLTS - Open Library of Technical Specifications
+ * Copyright (C) 2013 Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 BOLTS_THREAD_COLOR = [255,255,0];
 
 BOLTS_MODE = "sketch";
