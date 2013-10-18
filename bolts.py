e29bd21ad151021352f1c7be6410d6011b7eed15
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-10-18 14:48:30 +0200

    release subcommand for bolts.py

--- bolts.py
+++ bolts.py
@@ -17,23 +17,18 @@
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 from bolttools import blt_parser, openscad, freecad, html, downloads
+from bolttools.license import LICENSES_SHORT
 from os import getcwd
+from shutil import make_archive
 import os.path
-from subprocess import Popen
+from subprocess import call, Popen
 import argparse
+from datetime import datetime
 
-licenseids = {
-	"lgpl2.1" : "LGPL 2.1",
-	"lgpl2.1+" : "LGPL 2.1+",
-	"lgpl3" : "LGPL 3.0",
-	"lgpl3+" : "LGPL 3.0+",
-	"gpl3" : "GPL 3.0",
-	"gpl3+" : "GPL 3.0+"
-}
 
 def export(args):
 	repo = blt_parser.BOLTSRepository(args.repo)
-	license = licenseids[args.license]
+	license = LICENSES_SHORT[args.license]
 	if args.target == "openscad":
 		openscad.OpenSCADExporter().write_output(repo,license)
 	elif args.target == "freecad":
@@ -43,13 +38,51 @@ def export(args):
 
 def test(args):
 	if args.target == "freecad":
-		exec_dir = os.path.split(args.repo)[0]
+		exec_dir = os.path.join(args.repo,"output","freecad")
 		freecad_process = Popen(["freecad","-M","."],cwd=exec_dir)
 		freecad_process.wait()
 	elif args.target == "openscad":
 		exec_dir = os.path.join(args.repo,"output","openscad")
 		freecad_process = Popen(["openscad"],cwd=exec_dir)
 		freecad_process.wait()
+
+def release(args):
+	#check that there are no uncommited changes
+	if call(["git","diff","--exit-code","--quiet"]) == 1:
+		print "There are uncommited changes present in the git repository. Please take care of them before releasing"
+		exit(1)
+
+	if args.kind == "stable" and args.version is None:
+		print "Please specify a version using -v when releasing a stable version"
+		exit(2)
+	if args.kind == "development" and not args.version is None:
+		print "No explicit version can be given for development releases"
+		exit(2)
+	repo = blt_parser.BOLTSRepository(args.repo)
+
+	#export
+	html.HTMLExporter().write_output(repo)
+	for li_short in ["lgpl2.1+","gpl3"]:
+		license = LICENSES_SHORT[li_short]
+		openscad.OpenSCADExporter().write_output(repo,license)
+		freecad.FreeCADExporter().write_output(repo,license)
+
+		for backend,backend_name in zip(["freecad","openscad"],["FreeCAD","OpenSCAD"]):
+			#construct filename from date
+			if args.kind == "development":
+				date = datetime.now().strftime("%Y%m%d%H%M")
+				template = "BOLTS_%s_%s_%s" % (backend_name,date,li_short)
+			elif args.kind == "stable":
+				template = "BOLTS_%s_%s_%s" % (backend_name,args.version,li_short)
+
+			#create archives
+			root_dir = os.path.join(repo.path,"output",backend)
+			base_name = os.path.join(repo.downloads.out_root,"downloads",backend,template)
+			make_archive(base_name,"gztar",root_dir)
+			make_archive(base_name,"zip",root_dir)
+
+	#write html overview
+	downloads.DownloadsExporter().write_output(repo)
 
 
 parser = argparse.ArgumentParser()
@@ -77,6 +110,16 @@ parser_test.add_argument("target",
 	choices=["openscad","freecad"],
 	help="the backend to test")
 parser_test.set_defaults(func=test)
+
+parser_release = subparsers.add_parser("release")
+parser_release.set_defaults(func=release)
+parser_release.add_argument("kind",
+	type=str,
+	choices=["development","stable"],
+	help="whether to create a development snapshot or a official relase",
+	default="development")
+parser_release.add_argument("-v","--version",type=str)
+
 
 args = parser.parse_args()
 args.func(args)
