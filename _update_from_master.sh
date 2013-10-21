fc18f96840eaf0daf2b721ae58c4123d85e72247
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-10-21 12:34:53 +0200

    fixed missing image in spec
    and add changes automatically to git when updating

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -8,3 +8,5 @@ git rm -rfq output
 git add html downloads downloads.html
 git add _includes/statistics.html
 rst2html --template _spec_template.txt bolttools/doc/blt_spec_0_2.rst > doc/general/specification.html
+cp bolttools/doc/processing.* doc/general
+git add doc/general/processing.* doc/general/specification.html
