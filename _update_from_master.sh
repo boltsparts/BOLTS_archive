fdce63194de225a161d751f3f09c8138cb3f46e0
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-10-21 12:01:44 +0200

    automatic conversion of current spec to html

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -7,3 +7,4 @@ mv html/statistics.html _includes
 git rm -rfq output
 git add html downloads downloads.html
 git add _includes/statistics.html
+rst2html --template _spec_template.txt bolttools/doc/blt_spec_0_2.rst > doc/general/specification.html
