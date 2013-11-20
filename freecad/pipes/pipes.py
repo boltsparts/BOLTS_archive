92ade98eb3e806c18c290e3fc4b98a6e69158e15
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-11-20 09:25:51 +0100

    added freecad base for pipes

---
+++ freecad/pipes/pipes.py
@@ -0,0 +1,29 @@
+#BOLTS - Open Library of Technical Specifications
+#Copyright (C) 2013 Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or any later version.
+#
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import Part
+
+def pipe(params,document):
+	id = params['id']
+	od = params['od']
+	l = params['l']
+	name = params['name']
+
+	part = document.addObject("Part::Feature",name)
+	outer = Part.makeCylinder(0.5*od,l)
+	inner = Part.makeCylinder(0.5*id,l)
+	part.Shape = outer.cut(inner).removeSplitter()
