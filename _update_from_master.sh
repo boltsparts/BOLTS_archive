96129ef229d76dbf8df61b7e76101ad805632fa0
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-11-28 16:55:09 +0100

    adapted update script to new html output structure

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-git checkout master -- output
-git rm -rfq html downloads
-mv output/html .
-mv output/downloads/* .
-mv html/statistics.html _includes
+git checkout master -- output downloads
+git rm -rfq html
+mv output/html/* .
+git add html
 git rm -rfq output
-git add html downloads downloads.html
-git add _includes/statistics.html
-rst2html --template _spec_template.txt bolttools/doc/blt_spec_0_2.rst > doc/general/specification.html
-cp bolttools/doc/processing.* doc/general
-git add doc/general/processing.* doc/general/specification.html
+git add html index.html
+#rst2html --template _spec_template.txt bolttools/doc/blt_spec_0_2.rst > doc/general/specification.html
+#cp bolttools/doc/processing.* doc/general
+#git add doc/general/processing.* doc/general/specification.html
