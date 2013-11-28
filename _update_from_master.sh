29de36fe4f45fd8f15eeceb203da4c9f19890943
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2013-11-28 17:40:51 +0100

    also take drawing into account

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -4,7 +4,7 @@ git rm -rfq html
 mv output/html/* .
 git add html
 git rm -rfq output
-git add html *.html *.xml
+git add html *.html *.xml *.png
 #rst2html --template _spec_template.txt bolttools/doc/blt_spec_0_2.rst > doc/general/specification.html
 #cp bolttools/doc/processing.* doc/general
 #git add doc/general/processing.* doc/general/specification.html
