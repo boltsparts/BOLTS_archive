f390b2f580f0e3acc516e684c17967dde9c03cca
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-04-22 11:58:02 +0200

    auto update ofdevelopment version of specification

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -5,6 +5,6 @@ mv output/html/* .
 git add html
 git rm -rfq output
 git add html *.html *.xml *.png
-#rst2html --template _spec_template.txt bolttools/doc/blt_spec_0_2.rst > doc/general/specification.html
+git show master:bolttools/doc/blt_spec_dev.rst | rst2html --template _spec_template.txt > doc/general/specification.html
 #cp bolttools/doc/processing.* doc/general
 #git add doc/general/processing.* doc/general/specification.html
