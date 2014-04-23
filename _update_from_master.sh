3f38e67e2b89f8ef02068ddcde201ae35731e177
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-04-23 20:50:18 +0200

    fixed automatic spec page update

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -5,6 +5,6 @@ mv output/html/* .
 git add html
 git rm -rfq output
 git add html *.html *.xml *.png
-git show master:bolttools/doc/blt_spec_dev.rst | rst2html --template _spec_template.txt > doc/general/specification.html
+git show master:bolttools/doc/blt_spec_dev.rst | rst2html --template _spec_template.txt > doc/general/specification_dev.html
 #cp bolttools/doc/processing.* doc/general
 #git add doc/general/processing.* doc/general/specification.html
