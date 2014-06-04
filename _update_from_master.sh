929c8ca41c50d831c778ed753f61fef83ca7b149
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-06-04 12:45:20 +0200

    also update thingtracker

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -4,7 +4,7 @@ rm -rf html
 mv output/html/* .
 git add html
 git rm -rfq output
-git add html *.html *.xml *.png
+git add html *.html *.xml *.png *.json
 git show master:bolttools/doc/blt_spec_dev.rst | rst2html --template _spec_template.txt > doc/general/specification_dev.html
 #cp bolttools/doc/processing.* doc/general
 #git add doc/general/processing.* doc/general/specification.html
