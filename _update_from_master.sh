26a4c46677d67a857db1914b7bc3591dffc952a9
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-06-06 17:58:25 +0200

    only add important html files

--- _update_from_master.sh
+++ _update_from_master.sh
@@ -4,7 +4,7 @@ rm -rf html
 mv output/html/* .
 git add html
 git rm -rfq output
-git add html *.html *.xml *.png *.json
+git add html blog.html contribute.html contributors.html downloads.html index.html public_domain.html tasks.html unclear_license.html atom.xml no_drawing.png thingtracker.json
 git show master:bolttools/doc/blt_spec_dev.rst | rst2html --template _spec_template.txt > doc/general/specification_dev.html
 #cp bolttools/doc/processing.* doc/general
 #git add doc/general/processing.* doc/general/specification.html
