a237a6992ea8609be21ea062be6baa681a86cd16
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-07-22 09:10:02 +0200

    more or less complete docs blueprint

--- website/docs/__init__.py
+++ website/docs/__init__.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, abort, redirect, request, url_for
-from os.path import exists,join,relpath
+from os.path import exists,relpath, splitext, join
+from flask.helpers import safe_join, send_from_directory
 from yaml import safe_load as load
+from urlparse import urljoin
 from os import walk, listdir
+import re
 
 docs = Blueprint("docs",__name__,template_folder="templates",static_folder="static")
 
@@ -14,7 +17,7 @@ def split_yaml_header(fid):
 		if line.strip() == '---':
 			break
 		header.append(line)
-	content = '\n'.join(line for line in fid)
+	content = ''.join(line for line in fid)
 	return load('\n'.join(header)),content
 
 class Documentation:
@@ -30,12 +33,14 @@ class Documentation:
 			self.categories.append(cat)
 
 			for filename in filenames:
+				if filename.startswith('.'):
+					continue
 				doc = {}
 				doc["category"] = cat
 				with open(join(dirpath,filename)) as fid:
 					header, content = split_yaml_header(fid)
 				doc["title"] = header["title"]
-				doc["filename"] = filename
+				doc["filename"] = splitext(filename)[0]
 				doc["audience"] = header["audience"]
 				self.audiences.append(header["audience"])
 				doc["content"] = content
@@ -50,6 +55,7 @@ class Documentation:
 		return self.audiences
 
 	def get_documents(self,category=None,audience=None,filename=None):
+		res = []
 		for doc in self.documents:
 			if (not category is None) and doc["category"] != category:
 				continue
@@ -57,7 +63,8 @@ class Documentation:
 				continue
 			if (not filename is None) and doc["filename"] != filename:
 				continue
-			yield doc
+			res.append(doc)
+		return res
 
 SOURCES = {}
 STABLE = 0
@@ -68,12 +75,14 @@ for version in listdir(join(docs.root_path,"sources")):
 		pass
 	SOURCES[version] = Documentation(join(docs.root_path,"sources",version))
 
+@docs.route("/static/<version>/<filename>")
+def static_version(version,filename):
+	return send_from_directory(docs.static_folder,safe_join(version,filename))
 
 @docs.route("/")
 @docs.route("/index.html")
 def home():
 	return redirect(url_for(".index",version=STABLE))
-
 
 @docs.route("/<version>")
 @docs.route("/<version>/index.html")
@@ -85,22 +94,17 @@ def index(version):
 	for aud in src.get_audiences():
 		doc_structure[aud] = {}
 		for cat in src.get_categories():
-			doc_structure[aud][cat] = list(src.get_documents(cat,aud))
+			doc_structure[aud][cat] = list(src.get_documents(category=cat,audience=aud))
 	page = {"title" : "Documentation", "stable" : STABLE, "version" : version}
 	return render_template("doc.html",page=page,auds=doc_structure)
 
 @docs.route("/<version>/<cat>/<filename>")
 @docs.route("/<version>/<cat>/<filename>.html")
 def document(version,cat,filename):
-	print version,cat,filename
-	print SOURCES
 	if not version in SOURCES:
 		abort(404)
 	src = SOURCES[version]
-	print src.get_categories()
-	if not cat.lower() in [cat.lower() for cat in src.get_categories()]:
-		abort(404)
-	doc = list(src.get_documents(cat,filename=filename))
+	doc = list(src.get_documents(category=cat,filename=filename))
 	if len(doc) != 1:
 		abort(404)
 	page = {"title" : "Documentation", "stable" : STABLE, "version" : version}
