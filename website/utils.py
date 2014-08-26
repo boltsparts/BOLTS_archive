577d973f10cce013e8af5f2a269ccb0c1bd9fde4
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-08-26 14:49:02 +0200

    fixed lost code sections in documentation

--- website/utils.py
+++ website/utils.py
@@ -124,7 +124,7 @@ class Documentation:
 
 					self.audiences.add(header["audience"])
 
-					doc["content"] = [ p.strip().replace("%","%%") for p in content.split('\n\n')]
+					doc["content"] = [ p.replace("%","%%") for p in content.split('\n\n')]
 					self.documents.append(doc)
 
 		self.categories = list(self.categories)
