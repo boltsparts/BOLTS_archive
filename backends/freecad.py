4cafdf15dac2b065510ee777e4dc920a2454aed5
Bernd Hahnebach <bernd@bimstatik.org>
2020-02-13 08:02:00 +0100

    freecad backend, code formating E231, space after ,

--- backends/freecad.py
+++ backends/freecad.py
@@ -26,76 +26,76 @@ from . import license
 from .errors import *
 
 class FreeCADBackend(Backend):
-    def __init__(self,repo,databases):
-        Backend.__init__(self,repo,"freecad",databases,["freecad"])
-    def write_output(self,out_path,**kwargs):
-        args = self.validate_arguments(kwargs,["target_license","version"])
+    def __init__(self, repo, databases):
+        Backend.__init__(self, repo, "freecad", databases, ["freecad"])
+    def write_output(self, out_path, **kwargs):
+        args = self.validate_arguments(kwargs, ["target_license", "version"])
 
         self.clear_output_dir(out_path)
-        bolts_path = join(out_path,"BOLTS")
+        bolts_path = join(out_path, "BOLTS")
 
         # generate macro
-        start_macro = open(join(out_path,"start_bolts.FCMacro"),"w")
+        start_macro = open(join(out_path, "start_bolts.FCMacro"), "w")
         start_macro.write("import BOLTS\n")
         start_macro.write("BOLTS.show_widget()\n")
         start_macro.close()
 
         # copy files
         # bolttools
-        if not license.is_combinable_with("LGPL 2.1+",args["target_license"]):
+        if not license.is_combinable_with("LGPL 2.1+", args["target_license"]):
             raise IncompatibleLicenseError(
                 "bolttools is LGPL 2.1+, which is not compatible with %s" % args["target_license"])
-        copytree(join(self.repo.path,"bolttools"),join(bolts_path,"bolttools"))
+        copytree(join(self.repo.path, "bolttools"), join(bolts_path, "bolttools"))
         # remove the test suite and documentation, to save space
-        rmtree(join(bolts_path,"bolttools","test_blt"))
+        rmtree(join(bolts_path, "bolttools", "test_blt"))
 
         # generate version file
         date = datetime.now()
-        version_file = open(join(bolts_path,"VERSION"),"w")
+        version_file = open(join(bolts_path, "VERSION"), "w")
         version_file.write("%s\n%d-%d-%d\n%s\n" %
             (args["version"], date.year, date.month, date.day, args["target_license"]))
         version_file.close()
 
         # freecad gui code
-        if not license.is_combinable_with("LGPL 2.1+",args["target_license"]):
+        if not license.is_combinable_with("LGPL 2.1+", args["target_license"]):
             raise IncompatibleLicenseError(
                 "FreeCAD gui files are LGPL 2.1+, which is not compatible with %s" % args["target_license"])
-        if not exists(join(bolts_path,"freecad")):
-            makedirs(join(bolts_path,"freecad"))
-        if not exists(join(bolts_path,"data")):
-            makedirs(join(bolts_path,"data"))
-        open(join(bolts_path,"freecad","__init__.py"),"w").close()
+        if not exists(join(bolts_path, "freecad")):
+            makedirs(join(bolts_path, "freecad"))
+        if not exists(join(bolts_path, "data")):
+            makedirs(join(bolts_path, "data"))
+        open(join(bolts_path, "freecad", "__init__.py"), "w").close()
 
-        copytree(join(self.repo.path,"backends","freecad","gui"),join(bolts_path,"gui"))
-        copytree(join(self.repo.path,"backends","freecad","assets"),join(bolts_path,"assets"))
-        copytree(join(self.repo.path,"icons"),join(bolts_path,"icons"))
-        copyfile(join(self.repo.path,"backends","freecad","init.py"),join(bolts_path,"__init__.py"))
-        open(join(bolts_path,"gui","__init__.py"),"w").close()
+        copytree(join(self.repo.path, "backends", "freecad", "gui"), join(bolts_path, "gui"))
+        copytree(join(self.repo.path, "backends", "freecad", "assets"), join(bolts_path, "assets"))
+        copytree(join(self.repo.path, "icons"), join(bolts_path, "icons"))
+        copyfile(join(self.repo.path, "backends", "freecad", "init.py"), join(bolts_path, "__init__.py"))
+        open(join(bolts_path, "gui", "__init__.py"), "w").close()
 
         # compile ui files
-        uic.compileUiDir(join(bolts_path,"gui"))
+        uic.compileUiDir(join(bolts_path, "gui"))
 
         for coll, in self.repo.itercollections():
-            if not license.is_combinable_with(coll.license_name,args["target_license"]):
+            if not license.is_combinable_with(coll.license_name, args["target_license"]):
                 continue
-            copy(join(self.repo.path,"data","%s.blt" % coll.id),
-                join(bolts_path,"data","%s.blt" % coll.id))
+            copy(join(self.repo.path, "data", "%s.blt" % coll.id),
+                join(bolts_path, "data", "%s.blt" % coll.id))
 
-            if not exists(join(bolts_path,"freecad",coll.id)):
-                makedirs(join(bolts_path,"freecad",coll.id))
+            if not exists(join(bolts_path, "freecad", coll.id)):
+                makedirs(join(bolts_path, "freecad", coll.id))
 
-            if not exists(join(self.repo.path,"freecad",coll.id,"%s.base" % coll.id)):
+            if not exists(join(self.repo.path, "freecad", coll.id, "%s.base" % coll.id)):
                 continue
 
-            copy(join(self.repo.path,"freecad",coll.id,"%s.base" % coll.id),
-                join(bolts_path,"freecad",coll.id,"%s.base" % coll.id))
+            copy(join(self.repo.path, "freecad", coll.id, "%s.base" % coll.id),
+                join(bolts_path, "freecad", coll.id, "%s.base" % coll.id))
 
-            open(join(bolts_path,"freecad",coll.id,"__init__.py"),"w").close()
+            open(join(bolts_path, "freecad", coll.id, "__init__.py"), "w").close()
 
             for base, in self.dbs["freecad"].iterbases(filter_collection=coll):
                 if base.license_name not in license.LICENSES:
                     continue
-                if not license.is_combinable_with(base.license_name,args["target_license"]):
+                if not license.is_combinable_with(base.license_name, args["target_license"]):
                     continue
-                copy(join(self.repo.path,"freecad",coll.id,basename(base.filename)),
-                    join(bolts_path,"freecad",coll.id,basename(base.filename)))
+                copy(join(self.repo.path, "freecad", coll.id, basename(base.filename)),
+                    join(bolts_path, "freecad", coll.id, basename(base.filename)))
