da36722dc329e2e68485cc5b80e48af83ebaf8b8
Bernd Hahnebach <bernd@bimstatik.org>
2020-10-21 05:03:50 +0200

    freecad gui backend code formating: E265 block comment should start with '# '

--- backends/freecad/gui/freecad_bolts.py
+++ backends/freecad/gui/freecad_bolts.py
@@ -1,19 +1,19 @@
-#BOLTS - Open Library of Technical Specifications
-#Copyright (C) 2013 Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
+# BOLTS - Open Library of Technical Specifications
+# Copyright (C) 2013 Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
 #
-#This library is free software; you can redistribute it and/or
-#modify it under the terms of the GNU Lesser General Public
-#License as published by the Free Software Foundation; either
-#version 2.1 of the License, or any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or any later version.
 #
-#This library is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#Lesser General Public License for more details.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-#You should have received a copy of the GNU Lesser General Public
-#License along with this library; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 import importlib
 import sys
@@ -84,7 +84,7 @@ def add_part(collection, base, params, doc):
     else:
         raise RuntimeError("Unknown base geometry type: %s" % type(base))
 
-#custom widgets
+# custom widgets
 
 class PropertyWidget(QPropertyWidget):
     def __init__(self, parent, prop, value):
@@ -196,7 +196,7 @@ class BoltsWidget(QBoltsWidget):
         self.std_root = QtGui.QTreeWidgetItem(self.ui.partsTree, ['Standard', 'Ordered by issuing body'])
         self.std_root.setData(0, 32, None)
 
-        #set up collections
+        # set up collections
         for coll, in self.repo.itercollections():
             coll_item = QtGui.QTreeWidgetItem(self.coll_root, [coll.name, coll.description])
             coll_item.setData(0, 32, coll)
@@ -205,7 +205,7 @@ class BoltsWidget(QBoltsWidget):
             multistds = {}
 
             clasids = []
-            #names
+            # names
             for name, multiname in self.dbs["freecad"].iternames(['name', 'multiname'], filter_collection=coll):
                 clasids.append(self.repo.class_names.get_src(name).id)  # append classid
                 item = None
@@ -218,7 +218,7 @@ class BoltsWidget(QBoltsWidget):
 
                 item.setData(0, 32, name)
 
-            #single names
+            # single names
             for std, multistd in self.dbs["freecad"].iterstandards(['standard', 'multistandard'], filter_collection = coll):
                 item = None
                 if self.repo.class_standards.get_src(std).id not in clasids:  # only add item if it is not in classids
@@ -233,11 +233,11 @@ class BoltsWidget(QBoltsWidget):
 
         multistds = {}
 
-        #set up standards
+        # set up standards
         for body, in repo.iterbodies():
             std_item = QtGui.QTreeWidgetItem(self.std_root, [body.body, "Standards issued by %s" % body.body])
             std_item.setData(0, 32, None)
-            #single standards
+            # single standards
             for std, multistd in self.dbs["freecad"].iterstandards(['standard', 'multistandard'], filter_body = body):
                 if multistd is None:
                     item = QtGui.QTreeWidgetItem(std_item, [std.standard.get_nice(), std.description])
@@ -259,7 +259,7 @@ class BoltsWidget(QBoltsWidget):
                 root_item.removeChild(child)
 
     def setup_param_widgets(self, cl, base):
-        #construct widgets
+        # construct widgets
         params = cl.parameters.union(base.parameters)
 
         for p in params.free:
@@ -281,7 +281,7 @@ class BoltsWidget(QBoltsWidget):
                 raise ValueError("Unknown type encountered for parameter %s: %s" % (p, p_type))
             self.param_widgets[p].setToolTip(params.description[p])
 
-            #add them to layout
+            # add them to layout
             self.ui.param_layout.addWidget(self.param_widgets[p])
         if base.type == "fcstd":
             self.ui.addButton.setText("Add part (may take a bit)")
@@ -289,18 +289,18 @@ class BoltsWidget(QBoltsWidget):
             self.ui.addButton.setText("Add part")
 
     def setup_props_collection(self, coll):
-        #construct widgets
+        # construct widgets
         self.props_widgets.append(PropertyWidget(self.ui.props, "Name", coll.name))
         self.props_widgets.append(PropertyWidget(self.ui.props, "Description", coll.description))
         self.props_widgets.append(PropertyWidget(self.ui.props, "Authors", ", ".join(coll.author_names)))
         self.props_widgets.append(PropertyWidget(self.ui.props, "License", coll.license_name))
 
-        #add them to layout
+        # add them to layout
         for widget in self.props_widgets:
             self.ui.props_layout.addWidget(widget)
 
     def setup_props_standard(self, std):
-        #construct widgets
+        # construct widgets
         self.props_widgets.append(PropertyWidget(self.ui.props, "Name", std.standard.get_nice()))
         if std.description:
             self.props_widgets.append(PropertyWidget(self.ui.props, "Description", std.description))
@@ -314,18 +314,18 @@ class BoltsWidget(QBoltsWidget):
             self.props_widgets.append(PropertyWidget(self.ui.props, "Replacedby", std.replacedby))
         self.props_widgets.append(PropertyWidget(self.ui.props, "ID", std.get_id()))
 
-        #add them to layout
+        # add them to layout
         for widget in self.props_widgets:
             self.ui.props_layout.addWidget(widget)
 
     def setup_props_name(self, name):
-        #construct widgets
+        # construct widgets
         self.props_widgets.append(PropertyWidget(self.ui.props, "Name", name.name.get_nice()))
         if name.description:
             self.props_widgets.append(PropertyWidget(self.ui.props, "Description", name.description))
         self.props_widgets.append(PropertyWidget(self.ui.props, "ID", name.get_id()))
 
-        #add them to layout
+        # add them to layout
         for widget in self.props_widgets:
             self.ui.props_layout.addWidget(widget)
 
@@ -349,7 +349,7 @@ class BoltsWidget(QBoltsWidget):
             return
 
         params = {}
-        #read parameters from widgets
+        # read parameters from widgets
         for key in self.param_widgets:
             params[key] = self.param_widgets[key].getValue()
         params = cl.parameters.collect(params)
@@ -361,7 +361,7 @@ class BoltsWidget(QBoltsWidget):
         for key, tp in cl.parameters.types.items():
             if tp in lengths:
                 if params[key] is None:
-                    #A undefined value is not necessarily fatal
+                    # A undefined value is not necessarily fatal
                     continue
                 if hasattr(FreeCAD.Units, "parseQuantity"):
                     params[key] = FreeCAD.Units.parseQuantity("%g %s" %
@@ -370,7 +370,7 @@ class BoltsWidget(QBoltsWidget):
                     params[key] = FreeCAD.Units.translateUnit("%g %s" %
                         (params[key], lengths[tp]))
 
-        #add part
+        # add part
         try:
             base = self.dbs["freecad"].base_classes.get_src(cl)
             coll = self.repo.collection_classes.get_src(cl)
@@ -391,12 +391,12 @@ class BoltsWidget(QBoltsWidget):
         item = items[0]
         data = unpack(item.data(0, 32))
 
-        #clear props widget
+        # clear props widget
         for widget in self.props_widgets:
             self.ui.props_layout.removeWidget(widget)
             widget.setParent(None)
         self.props_widgets = []
-        #clear
+        # clear
         for key in self.param_widgets:
             self.ui.param_layout.removeWidget(self.param_widgets[key])
             self.param_widgets[key].setParent(None)
