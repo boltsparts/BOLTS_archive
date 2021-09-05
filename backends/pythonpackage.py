3475b1d5db2590730f3d81b0dc537f6f93adb03e
Bernd Hahnebach <bernd@bimstatik.org>
2021-09-05 21:39:30 +0200

    pythonpackage and freecad backend, generalize data and geometric module creation data

--- backends/pythonpackage.py
+++ backends/pythonpackage.py
@@ -36,51 +36,36 @@ class PythonPackageBackend(Backend):
         Backend.__init__(self, repo, "pythonpackage", databases, ["pythonpackage"])
 
     def write_output(self, out_path, **kwargs):
-        args = self.validate_arguments(kwargs, ["target_license", "version"])
+        self.args = self.validate_arguments(kwargs, ["target_license", "version"])
+        self.license = license
 
         self.clear_output_dir(out_path)
 
-        bolts_path = join(out_path, "boltspy")
-        makedirs(bolts_path)
+        self.bout_path = join(out_path, "boltspy")
+        makedirs(self.bout_path)
 
         # generate version file
         date = datetime.now()
-        with open(join(bolts_path, "VERSION"), "w") as version_file:
+        with open(join(self.bout_path, "VERSION"), "w") as version_file:
             version_file.write("%s\n%d-%d-%d\n%s\n" %
-                (args["version"], date.year, date.month, date.day, args["target_license"]))
+                (self.args["version"], date.year, date.month, date.day, self.args["target_license"]))
 
         # copy bolttools files
-        if not license.is_combinable_with("LGPL 2.1+", args["target_license"]):
+        if not self.license.is_combinable_with("LGPL 2.1+", self.args["target_license"]):
             raise IncompatibleLicenseError(
-                "bolttools is LGPL 2.1+, which is not compatible with %s" % args["target_license"])
-        copytree(join(self.repo.path, "bolttools"), join(bolts_path, "bolttools"))
+                "bolttools is LGPL 2.1+, which is not compatible with %s" % self.args["target_license"])
+        copytree(join(self.repo.path, "bolttools"), join(self.bout_path, "bolttools"))
         # remove the test suite and documentation, to save space
-        rmtree(join(bolts_path, "bolttools", "test_blt"))
+        rmtree(join(self.bout_path, "bolttools", "test_blt"))
 
         copyfile(
             join(self.repo.path, "backends", "pythonpackage", "init.py"),
-            join(bolts_path, "__init__.py")
+            join(self.bout_path, "__init__.py")
         )
         copyfile(
             join(self.repo.path, "backends", "common", "repo_tools.py"),
-            join(bolts_path, "repo_tools.py")
+            join(self.bout_path, "repo_tools.py")
         )
 
-        # copy part data
-        if not exists(join(bolts_path, "data")):
-            makedirs(join(bolts_path, "data"))
-        if not exists(join(bolts_path, "pythonpackage")):
-            makedirs(join(bolts_path, "pythonpackage"))
-
-        for coll, in self.repo.itercollections():
-
-            # copy all part data
-            # skip collection if licenses issues
-            if not license.is_combinable_with(coll.license_name, args["target_license"]):
-                print("Skip %s due to license issues" % coll.id)
-                continue
-
-            copy(
-                join(self.repo.path, "data", "%s.blt" % coll.id),
-                join(bolts_path, "data", "%s.blt" % coll.id)
-            )
+        # copy data and creator modules
+        self.copy_data_and_creator_modules(all_data=True)
